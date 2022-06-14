abcc4d78d5bf0f51cf0410ef2ca3eded02b07c96
Kinox2000 <99295725+Kinox2000@users.noreply.github.com>
2022-06-13 20:43:21 -0600

    Add files via upload

--- AppInicializacion.py
+++ AppInicializacion.py
@@ -4,18 +4,14 @@ import tkinter as tk
 import time
 import sys 
 
-window = tk.Tk()
-window.geometry('400x153')
-mensaje = tk.Label(text="Aplicación de reconocimiento de letras")
-mensaje.pack()
 
 def Iniciar():
 
-    subprocess.run('ssh root@192.168.10.110 python3 /usr/bin/appreconocimiento', shell=True)
+    subprocess.run('ssh root@192.168.2.25 python3 /usr/bin/appreconocimiento', shell=True)
 
     time.sleep(10) 
     
-    subprocess.run('scp root@192.168.10.110:/usr/bin/Texto.txt /Users/ejcs2/Desktop', shell=True)
+    subprocess.run('scp root@192.168.2.25:/usr/bin/Letras.txt /Users/ejcs2/Desktop', shell=True)
 
 def Escuchar():
     s = pyttsx3.init()  
@@ -28,6 +24,10 @@ def Salir():
     window.destroy()
     sys.exit("Terminado")
 
+window = tk.Tk()
+window.geometry('400x153')
+mensaje = tk.Label(text="Aplicación de reconocimiento de letras")
+mensaje.pack()
 
 boton = tk.Button(text="Iniciar la aplicación", command = Iniciar)
 boton.place(x=145, y=30)
@@ -38,3 +38,4 @@ boton2.place(x=170, y=70)
 boton3 = tk.Button(text= "Salir", command = Salir)
 boton3.place(x=180, y=110)
 
+window.mainloop()
